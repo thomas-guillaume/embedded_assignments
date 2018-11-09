aa44bf3ff14439c7d015aae8097000089fdbb984
Thomas Guillaume <thomaseq.guillaume@gmail.com>
2018-11-09 11:42:22 +0800

    style: improve the news page

--- GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/NewsFragment.java
+++ GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/NewsFragment.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class NewsFragment extends Fragment {
+
+    private ListView news_layout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View v = inflater.inflate(R.layout.fragment_news, container, false);
+
+        news_layout = (ListView)v.findViewById(R.id.news_layout);
+
+        return v;
     }
 }
