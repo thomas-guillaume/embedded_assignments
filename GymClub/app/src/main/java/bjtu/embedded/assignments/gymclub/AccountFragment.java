a3577fa0e9a7007e20d5e89dbe6712d31bbb6973
Thomas Guillaume <thomaseq.guillaume@gmail.com>
2018-11-09 11:28:05 +0800

    style: improve account page

--- GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/AccountFragment.java
+++ GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/AccountFragment.java
@@ -1,17 +1,44 @@
 package bjtu.embedded.assignments.gymclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AccountFragment extends Fragment {
+
+    private Button settings_btn;
+    private Button logout_btn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        settings_btn = (Button)v.findViewById(R.id.settings_btn);
+        logout_btn = (Button)v.findViewById(R.id.logout_btn);
+
+        settings_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SignUpSignInActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return v;
     }
 }
