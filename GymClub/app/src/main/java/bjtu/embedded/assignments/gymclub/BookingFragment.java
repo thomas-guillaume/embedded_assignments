efddf926ed57b89599b713145a6fde60be90247d
Thomas Guillaume <thomaseq.guillaume@gmail.com>
2018-11-09 11:38:34 +0800

    style: improve the booking page and create the results page

--- GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/BookingFragment.java
+++ GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/BookingFragment.java
@@ -1,17 +1,33 @@
 package bjtu.embedded.assignments.gymclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class BookingFragment extends Fragment {
+
+    private Button search_btn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        View v = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        search_btn = (Button)v.findViewById(R.id.search_btn);
+
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ResultsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
---
+++ GymClub/app/src/main/java/bjtu/embedded/assignments/gymclub/ResultsActivity.java
@@ -0,0 +1,13 @@
+package bjtu.embedded.assignments.gymclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
